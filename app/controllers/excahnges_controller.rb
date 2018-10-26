3ed2e324c8da4e1d298ac7b9f1d9a80cd4cc0947
Gumtape <gumtape018@gmail.com>
2018-10-27 00:45:51 +0900

    交換表QR削除

--- app/controllers/excahnges_controller.rb
+++ app/controllers/excahnges_controller.rb
@@ -1,32 +1,40 @@
 class ExchangesController < ApplicationController
-    # GET /cources
-    def index
-      @exchanges = exchange.all
+  # GET /cources
+  def index
+    @exchanges = Exchange.all
 
-      render 'index', formats: 'json', handlers: 'jbuilder'
-    end
-  
-    # GET /cources/:name
-    def show
-      @exchange = Exchange.find(params[:id])
-      
-      render 'show', formats: 'json', handlers: 'jbuilder'
-    end
-  
-    # POST /cources
-    def create
-      @exchange = Exchange.new(exchange_params)
+    render json: @exchanges
+  end
 
-      if @exchange.save
-        render json: @exchange, status: :created, location: @exchange_new
-      else
-        render json: @exchange.errors, status: :unprocessable_entity
-      end
-    end
-  
-    private
-    # Only allow a trusted parameter "white list" through.
-    def exchange_params
-      params.fetch(:exchange, {}).permit(:user_id, :course_id, :QR)
+  # GET /cources/:name
+  def show
+    @exchange = Exchange.find(params[:id])
+    
+    render 'show', formats: 'json', handlers: 'jbuilder'
+  end
+
+  # POST /cources
+  def create
+    @exchange = Exchange.new(exchange_params)
+    @exchange.QR = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(15).join
+
+    if @exchange.save
+      render json: @exchange, status: :created, location: @exchange_new
+    else
+      render json: @exchange.errors, status: :unprocessable_entity
     end
   end
+
+  def delete
+    @exchange = Exchange.find_by(user_id: params[:user_id], course_id: params[:course_id])
+    @exchange.update(exchange_params)
+    @result = 'success!'
+    render 'delete', formats: 'json', handlers: 'jbuilder'
+  end
+
+  private
+  # Only allow a trusted parameter "white list" through.
+  def exchange_params
+    params.fetch(:exchange, {}).permit(:user_id, :course_id, :QR)
+  end
+end
